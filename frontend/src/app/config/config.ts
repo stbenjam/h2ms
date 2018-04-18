1eaa10571c266d8125f985c727b7d26593bbb214
Wesley Skillern <wskillern@g.harvard.edu>
2018-04-18 18:44:36 -0400

    - before rebase

--- frontend/src/app/config/config.ts
+++ frontend/src/app/config/config.ts
@@ -8,7 +8,7 @@ export class ConfigQuestion {
  * Config file to switch between different types of applications. Ex. Hand Hygiene, Blue Gloves, or Blue Masks.
  */
 export class Config {
-    servicesReturnFakeData = false;
+    servicesReturnFakeData = true;
 
     appName: string;
     frontendHostname: string;
