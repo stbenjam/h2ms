d339724ca1a6d1baa685ad0212a0a3fed2dd6631
Wesley Skillern <wskillern@g.harvard.edu>
2018-04-24 20:49:05 -0400

    - able to get roles?

---
+++ frontend/src/app/user/service/user-role-resolver.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
+import {RoleEntityService} from '../..';
+
+@Injectable()
+export class UserRoleResolverService {
+
+    constructor(private roleEntityService: RoleEntityService) { }
+
+    resolve(route: ActivatedRouteSnapshot, rstate: RouterStateSnapshot, href: string): Observable<any> {
+        return this.roleEntityService.findAllRoleUsingGET(href, '10', undefined);
+    }
+
+}
