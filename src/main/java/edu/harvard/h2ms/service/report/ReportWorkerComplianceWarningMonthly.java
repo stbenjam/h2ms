2954b9c7e729c95ecef6aa639b9d30c7bc38709c
shinwachi <shinwachi@gmail.com>
2018-04-28 13:58:05 -0400

    Added @Component

--- src/main/java/edu/harvard/h2ms/service/report/ReportWorkerComplianceWarningMonthly.java
+++ src/main/java/edu/harvard/h2ms/service/report/ReportWorkerComplianceWarningMonthly.java
@@ -1,7 +1,9 @@
 package edu.harvard.h2ms.service.report;
 
 import edu.harvard.h2ms.service.utils.ReportUtils.NotificationFrequency;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ReportWorkerComplianceWarningMonthly extends AbstractReportWorkerComplianceWarning {
 
   long REPORTINGINTERVAL = NotificationFrequency.MONTHLY.seconds;
