642ac79a9cc52bda8b1f443227f778e611ee01c2
Stephen Benjamin <stephen@redhat.com>
2018-04-29 14:17:50 -0400

    Change default back to DAILY

--- src/main/java/edu/harvard/h2ms/service/report/NotificationServiceImpl.java
+++ src/main/java/edu/harvard/h2ms/service/report/NotificationServiceImpl.java
@@ -126,7 +126,7 @@ public class NotificationServiceImpl {
 
     // define how long to wait for each notification frequency
     if (notificationFrequency == NotificationFrequency.UNDEFINED) {
-      notificationFrequency = NotificationFrequency.HALFMINUTE;
+      notificationFrequency = NotificationFrequency.DAILY;
     }
 
     long interval = notificationFrequency.seconds;
