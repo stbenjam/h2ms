1f7a88e62cffb58dd9e8474a22e5f0028932fdcc
Wesley Skillern <wskillern@g.harvard.edu>
2018-04-29 18:07:20 -0400

    - in progress

--- frontend/src/app/auth/guards/event-guard.service.ts
+++ frontend/src/app/auth/guards/event-guard.service.ts
@@ -1,13 +1,12 @@
 import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
 import {AuthService} from '../auth.service';
-import {resolve} from 'url';
 import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class EventGuardService implements CanActivate {
 
-    hasObserverRole = false;
+    hasObserverRole = true;
 
     constructor(private router: Router,
                 private authService: AuthService) {
@@ -15,15 +14,13 @@ export class EventGuardService implements CanActivate {
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
         if (!this.authService.isLoggedIn()) {
-            alert('Please login to view this page.');
             this.router.navigate(['login']);
-            return resolve(false);
+            return false;
         }
         if (!this.hasObserverRole) {
-            alert('You do not have permissions to view this page.');
             this.router.navigate(['about']);
-            return resolve(false);
+            return false;
         }
-        return resolve(false);
+        return true;
     }
 }
