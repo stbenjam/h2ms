2aa33c4656bda338581903f92f1c9b4f0e817b81
Wesley Skillern <wskillern@g.harvard.edu>
2018-04-29 23:54:43 -0400

    - lock down mode activate!

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import {MediaMatcher} from '@angular/cdk/layout';
-import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
+import {ChangeDetectorRef, Component, OnChanges, OnDestroy, OnInit} from '@angular/core';
 import {ConfigService} from './config/config.service';
 import {Config} from './config/config';
 import {NavItem} from './sidenav/nav-item';
-import {NAV_ITEMS} from './app-routing.module';
+import {NAV_ITEMS_ADMIN, NAV_ITEMS_OBSERVER, NAV_ITEMS_USER} from './app-routing.module';
 import {Location} from '@angular/common';
 import {Title} from '@angular/platform-browser';
+import {AuthService} from './auth/auth.service';
+import {UserRoleService} from './user/service/user-role.service';
+import {ActivatedRouteSnapshot, RouterStateSnapshot} from "@angular/router";
 
 @Component({
     selector: 'app-root',
@@ -17,7 +20,7 @@ import {Title} from '@angular/platform-browser';
  * Main component for the App. The Navbar is based on this example:
  * https://stackblitz.com/angular/ngjvmobekyl?file=app%2Fsidenav-responsive-example.css
  */
-export class AppComponent implements OnDestroy {
+export class AppComponent implements OnDestroy, OnChanges {
     mobileQuery: MediaQueryList;
     config: Config;
     navItems: NavItem[];
@@ -28,18 +31,33 @@ export class AppComponent implements OnDestroy {
                 private media: MediaMatcher,
                 private location: Location,
                 private configService: ConfigService,
-                private titleService: Title) {
+                private titleService: Title,
+                private authService: AuthService,
+                private userRoleService: UserRoleService,
+                private route: ActivatedRouteSnapshot,
+                private state: RouterStateSnapshot) {
         this.mobileQuery = media.matchMedia('(max-width: 1050px)');
         this._mobileQueryListener = () => changeDetectorRef.detectChanges();
         this.mobileQuery.addListener(this._mobileQueryListener);
         this.config = configService.getConfig();
-        this.navItems = NAV_ITEMS;
+        this.navItems = NAV_ITEMS_USER;
         for (const navItem of this.navItems) {
             navItem.showSubItems = navItem.isCurrentlySelected(location.path());
         }
         this.setTitle(this.config.appName);
     }
 
+    ngOnChanges() {
+        if (this.authService.isLoggedIn()) {
+            if (this.userRoleService.hasRoles(['ROLE_ADMIN'], this.route, this.state)) {
+                this.setNavItems(NAV_ITEMS_ADMIN);
+            } else if (this.userRoleService.hasRoles(['ROLE_OBSERVER'], this.route, this.state)) {
+                this.setNavItems(NAV_ITEMS_OBSERVER);
+            } else {
+                this.setNavItems(NAV_ITEMS_USER);
+            }
+        }
+    }
 
     isInProduction() {
         return window.location.hostname === 'h2ms.org';
@@ -70,5 +88,11 @@ export class AppComponent implements OnDestroy {
         this.mobileQuery.removeListener(this._mobileQueryListener);
     }
 
+    public setNavItems(newNavItems: NavItem[]) {
+        this.navItems = newNavItems;
+        for (const navItem of this.navItems) {
+            navItem.showSubItems = navItem.isCurrentlySelected(this.location.path());
+        }
+    }
 
 }
