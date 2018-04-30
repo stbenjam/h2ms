3b482722c95cdaaf7606f301ca8a66f9c834a930
Kevin Trosinski <ktrosinski@g.harvard.edu>
2018-04-30 19:54:05 -0400

    Added case on roles-resolver for logged in users

--- frontend/src/app/role/service/role-resolver.service.ts
+++ frontend/src/app/role/service/role-resolver.service.ts
@@ -2,15 +2,20 @@ import { Injectable } from '@angular/core';
 import {RoleEntityService} from '../../';
 import {ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
 import {Observable} from 'rxjs/Observable';
+import {AuthService} from '../../auth/auth.service';
 
 @Injectable()
 export class RoleResolverService {
 
-    constructor(private roleEntityService: RoleEntityService) { }
+    constructor(private roleEntityService: RoleEntityService,
+                private authService: AuthService) { }
 
     resolve(route: ActivatedRouteSnapshot, rstate: RouterStateSnapshot): Observable<any> {
         // Get 50 roles
         // TODO: figure out how paging is going to work... and how many to get initially
+        if (!this.authService.isLoggedIn()) {
+            return Observable.of([]);
+        }
         return this.roleEntityService.findAllRoleUsingGET(undefined, '50', undefined);
     }
 
